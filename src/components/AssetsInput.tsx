20fab2385ed21acf654ebc4322ef9a5e163155ba
Sandeepani <h.sandeepani@gmail.com>
2025-12-30 20:41:17 +0530

    Add validations and validation styling

--- src/components/AssetsInput.tsx
+++ src/components/AssetsInput.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import type { Asset } from "../models/Asset";
+import React, { useState } from "react";
+import { AssetErrors, type Asset } from "../models/Asset";
 
 export default function AssetsInput(props: { onAddAsset: (asset: Asset) => void, asset: Asset, setAsset: (asset: Asset) => void, updateAsset: (asset: Asset) => void }) {
     const { onAddAsset, asset, setAsset, updateAsset } = props;
@@ -20,11 +20,38 @@ export default function AssetsInput(props: { onAddAsset: (asset: Asset) => void,
         setAsset({ ...asset, description });
     }
 
+    const [errors, setErrors] = useState<AssetErrors>({});
+
     function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
         console.log(asset.name, asset.price, asset.boughtAt, asset.description);
 
-        if (!asset.name || !asset.price || !asset.boughtAt) return;
+        function validateAsset() {
+            if (!asset.name || !asset.price || !asset.boughtAt) {
+                setErrors({
+                    name: !asset.name ? "Name is required" : "",
+                    price: !asset.price ? "Price is required" : "",
+                    boughtAt: !asset.boughtAt ? "Purchased on date is required" : "",
+                });
+                return false;
+            }
+            if (asset.boughtAt.toDateString() === "Invalid Date") {
+                setErrors({
+                    name: "",
+                    price: "",
+                    boughtAt: "Purchased on date is invalid",
+                });
+                return false;
+            }
+            setErrors({
+                name: "",
+                price: "",
+                boughtAt: "",
+            });
+            return true;
+        }
+
+        if (!validateAsset()) return;
 
         if (asset.id) {
             console.log(asset.name, asset.price, asset.boughtAt, asset.description);
@@ -61,9 +88,13 @@ export default function AssetsInput(props: { onAddAsset: (asset: Asset) => void,
                 <h2>Add New Asset</h2>
                 <div className="main-inputs">
                     <input type="text" placeholder="Enter asset name..." value={asset.name} onChange={(event) => setName(event.target.value)} />
+                    {errors.name && <div className="error"> <i className="fa-solid fa-circle-exclamation"></i> <p>{errors.name}</p> </div>}
                     <input type="number" placeholder="Asset price" value={asset.price} onChange={(event) => setPrice(Number(event.target.value))} />
+                    {errors.price && <div className="error"> <i className="fa-solid fa-circle-exclamation"></i> <p>{errors.price}</p> </div>}
                     <input type="date" placeholder="Asset purchased on" value={getBoughtAtDateString()} onChange={(event) => setBoughtAt(new Date(event.target.value))} />
+                    {errors.boughtAt && <div className="error"> <i className="fa-solid fa-circle-exclamation"></i> <p>{errors.boughtAt}</p> </div>}
                     <textarea placeholder="Add any additional details..." value={asset.description} onChange={(event) => setDescription(event.target.value)} />
+                    {errors.description && <div className="error"> <i className="fa-solid fa-circle-exclamation"></i> <p>{errors.description}</p> </div>}
                     <button id="submit" onClick={handleSubmit}><h3>{asset.id && asset.id !== -1 ? "Update" : "Add"} Asset</h3></button>
                 </div>
             </div>
--- src/models/Asset.ts
+++ src/models/Asset.ts
@@ -23,3 +23,28 @@ export class Asset {
         );
     }
 }
+
+export class AssetErrors {
+    name?: string;
+    price?: string;
+    boughtAt?: string;
+    description?: string;
+
+    // constructor(id: number, name: string, price: number, boughtAt: Date, description?: string) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.price = price;
+    //     this.boughtAt = boughtAt;
+    //     this.description = description;
+    // }
+
+    // static fromJSON(json: any): Asset {
+    //     return new Asset(
+    //         json.id,
+    //         json.name,
+    //         json.price,
+    //         new Date(json.boughtAt),
+    //         json.description
+    //     );
+    // }
+}
